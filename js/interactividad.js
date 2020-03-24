7e914b5d1d35a5b24189be55d09d7c4aae2ca84a
Juan Orjuela <orjuelajuans@gmail.com>
2020-03-24 17:30:45 -0500

    Proyecto 90%

--- js/interactividad.js
+++ js/interactividad.js
@@ -1,35 +1,40 @@
-$( document ).ready(function() {
+$(document).ready(function () {
   //Home
-    $( "#sl-btn" ).on( "click", function() {
-        $(this).toggleClass("is-active");
-        $(".sl-overlay").toggle();
-      });
+  $("#sl-btn").on("click", function () {
+    $(this).toggleClass("is-active");
+    $(".sl-overlay").toggle();
+  });
 
   // Plantas    
-  $("#nav-plantas").on("click", ".btn-planta", function(event){
+  $("#nav-plantas").on("click", ".btn-planta", function (event) {
     event.preventDefault();
     $(".btn-planta").removeClass("activo");
     $(this).addClass("activo");
     var planta = $(this).data("planta");
-    
+
     $("#plantas-cont").children("div").removeClass("activo");
-    $("#plantas-cont").children("."+planta).addClass("activo");
+    $("#plantas-cont").children("." + planta).addClass("activo");
   });
   //Transiciones
   $("a").on("click", redirigir);
 
-  function redirigir(e){
+  function redirigir(e) {
     e.preventDefault();
-    var link = $(this).attr("href");
-    var linkB = link.substr(0, link.indexOf('.'));
-    
-    var delay = 1200; // time in milliseconds
-    $(".tr-overlay").addClass(linkB).show(600);
-     
-    setTimeout(function(){
-     window.location = link;
-    },delay);
-    
-   }
+    if ($(this).hasClass("pesti") == false) {
+      var link = $(this).attr("href");
+      var linkB = link.substr(0, link.indexOf('.'));
+
+      var delay = 1200; // time in milliseconds
+      $(".tr-overlay").addClass(linkB).show(600);
+
+      setTimeout(function () {
+        window.location = link;
+      }, delay);
+    } else {
+      console.log("Todo bien")
+    }
+
+
+  }
 
 });
